1f766ffc854ce92a5f55f2e47df1eed94f9d17a4
Philip Meshach <philipmeshach.pm@gmail.com>
2022-08-30 10:10:59 +0530

    Final commit

--- src/main/java/com/niit/jdp/BankAccount.java
+++ src/main/java/com/niit/jdp/BankAccount.java
@@ -6,15 +6,39 @@
 package com.niit.jdp;
 
 public abstract class BankAccount {
+
     String accontHolderName;
     long accountNumber;
     String ifscCode;
     String branchName;
     double accountBalance;
+
     double widthDrawAmount;
     double depositeAmount;
     double transferFunds;
     String modeOfOperation;
     boolean internetBanking;
     double interestRate;
+
+    double toGetBalance() {
+
+        return accountBalance;
+    }
+
+    double debitCash() {
+        double interest = accountBalance * (interestRate / 100);
+        double cash = accountBalance - interest;
+        return cash;
+    }
+
+    double creditCash() {
+        double credit = accountBalance + 100;
+        return credit;
+    }
+
+}
+
+
+
+
 
--- src/main/java/com/niit/jdp/SavingsAccountImpl.java
+++ src/main/java/com/niit/jdp/SavingsAccountImpl.java
@@ -6,11 +6,29 @@
 package com.niit.jdp;
 
 public class SavingsAccountImpl {
-
-
-
     public void main(String[] args){
 
+        BankAccount bankAccount = new BankAccount() {
+            @Override
+            double toGetBalance() {
+                return super.toGetBalance();
+            }
 
+            @Override
+            double debitCash() {
+                return super.debitCash();
+            }
+
+            @Override
+            double creditCash() {
+                return super.creditCash();
+            }
+        };
+        double savingsAccountDebitCash = bankAccount.debitCash();
+        double savingsAccountCreditCash = bankAccount.creditCash();
+        double savingsAccountFundTransfer = bankAccount.toGetBalance();
+        System.out.println("Debit Cash: " +savingsAccountDebitCash);
+        System.out.println("savingsAccountCreditCash = " + savingsAccountCreditCash);
+        System.out.println("savingsAccountDebitCash = " + savingsAccountDebitCash);
     }
 }
